0837eaad1d58e3adf1735905d5a1084cf69a5724
NyitiG <61630823+NyitiG@users.noreply.github.com>
2021-09-04 14:43:13 +0200

    A kijelőlés klikk még nem teljesen működik, de a törlés, az oké

---  vscode-practice/Todo Lista haladó/script.js
+++  vscode-practice/Todo Lista haladó/script.js
@@ -1,31 +1,62 @@
-$("#add").click(() =>{
+let tpt=false
+$(document).ready(function(){
+    let listElementsFirst = "<li> <div class='liText'>"
+    let todoListText=""
+    let listElementsSecund = "</div><span class='remove'><i class='material-icons'>delete</i></span>"
+    let listElementsThird = "<div class='checkbox'>✓</div> </li>"
+
+$("#add").click(() =>{ 
+    todoListText=document.getElementById("inText").value
+    if (todoListText !="") {
+      $("ul").append(listElementsFirst+todoListText+listElementsSecund+listElementsThird) 
+      $(document).on('clik', '') 
+    }
+    
+    
+})
+$(document).on('click', '.checkbox', function() {
+
+    $(".checkbox").click (()=> {
+     if (tpt==false) {
+        $(this).css("background-color", "green")
+        $(this).css("color", "white")
+        $(this).parent().css("color","lightgrey")
+        $(".material-icons").css("color","black")
+        console.log("false ág lefutott")
+        } else {
+        $(this).css("background-color", "white")
+        $(this).css("color", "green")
+        $("li").css("color","black")
+        console.log("else ág lefutott")
+        }   
+        tpt=!tpt
+    })
+    
+})
+$(document).on('click', '.remove', function() {
+    $(this).parent().remove();
+})
+
+}
+)
+
+                        
+                         
+/*                         
+                     
+
+
     let szoveg =""
      szoveg=document.getElementById("inText").value
     $("ul").append("<li>"+szoveg+"<img src='img/trash.svg' alt='kuka'>"+"<div class='checkbox'>✓</div>"+'</li>') 
     $("div.checkbox").on('click',(event) => {
 
     })
-    szoveg=""
+   
 }
 )
-let ch=false
-$('.todoList > li > .checkbox').on('click', function() {
-    if (ch==false) {
-        $(this).css("background-color", "green")
-        $(this).css("color", "white")
-        $(this).parent().css("color","lightgrey")
-        $(".material-icons").css("color","black")
-        ch=true
-        console.log("if ág lefutott")
-        } else {
-        $(this).css("background-color", "white")
-        $(this).css("color", "green")
-        $("li").css("color","black")
-    
-        ch=false
-        console.log("else ág lefutott")
-        }
-            });
+
+
 
 /*let ch=false
 $(".checkbox").on('click', ()=>{
